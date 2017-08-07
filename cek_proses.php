00a28cd5b16919fe29b91f66db93142ef253628c
Andrea Damayanti <andr.dmynti22@gmail.com>
2017-08-07 19:58:28 +0700

    Update cek_proses.php

--- cek_proses.php
+++ cek_proses.php
@@ -29,7 +29,7 @@
                 }
 
 
-                $query  = mysqli_query($conn, "SELECT * FROM pesanan WHERE invoice='$invoice'")or die(mysqli_error($conn));
+                $query  = mysqli_query($conn, "SELECT pesanan.id_pesan, pesanan.nama, pesanan.id_bus, pesanan.id_berangkat, pesanan.tgl_pesan, pesanan.nik, pesanan.alamat, pesanan.tgl_berangkat, pesanan.total, pesanan.invoice, pesanan.konfirm, pesanan.respons, pesanan.pembayaran, keberangkatan.tujuan, keberangkatan.jadwal, bus.kelas, bus.harga FROM pesanan, bus, keberangkatan, kursi_pesanan, kursi WHERE pesanan.invoice = '$invoice'")or die(mysqli_error($conn));
 
                     if(mysqli_num_rows($query) == 0){   
                                 
@@ -42,7 +42,7 @@
 
                         //belum dikonfirmasi admin
                         if ($data['konfirm']==0) {
-                            echo "Pesanan Anda Belum Di konrimasi oleh admin, silahkan tunggu beberapa saat lagi";
+                            echo "Pesanan Anda Belum Di konfirmasi oleh admin, silahkan tunggu beberapa saat lagi";
                         }
                         //Sudah dikonfirmasi admin
                         elseif ($data['konfirm']==1 AND $data['respons']==''){
@@ -54,7 +54,62 @@
                         }
                         //cetak tiket
                         elseif ($data['konfirm']==1 AND $data['pembayaran']==1){
-                            echo "<span class='fa fa-print'></span> Cetak Tiket</a>";
+                            
+                        ?>
+                        <h4>Nama Pemesan : <?php echo $data['nama']; ?></h4>
+                        <br><br>
+                        <form action="config/update_konfirm.php" method="POST">
+                        <input type="hidden" name="id_pesan" value="<?php echo $data['id_pesan']; ?>">
+                            <table class="table table-striped-bordered">
+                                <tr>
+                                    <td width="200">Tanggal Pesan</td>
+                                    <td width="50">:</td>
+                                    <td><?php echo $data['tgl_pesan']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td width="200">NIK Pemesan</td>
+                                    <td width="50">:</td>
+                                    <td><?php echo $data['nik']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td width="200">Alamat</td>
+                                    <td width="50">:</td>
+                                    <td><?php echo $data['alamat']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td width="200">Tanggal Berangkat</td>
+                                    <td width="50">:</td>
+                                    <td><?php echo $data['tgl_berangkat']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td width="200">Jam Berangkat</td>
+                                    <td width="50">:</td>
+                                    <td><?php echo $data['jadwal']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td width="200">Tujuan</td>
+                                    <td width="50">:</td>
+                                    <td><?php echo $data['tujuan']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td width="200">Kelas</td>
+                                    <td width="50">:</td>
+                                    <td><?php echo $data['kelas']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td width="200">Harga</td>
+                                    <td width="50">:</td>
+                                    <td><?php echo $data['harga']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td width="200"></td>
+                                    <td><a href="admin.php?halaman=pesan_masuk" class="btn btn-warning">Batal</a></td>
+                                    <td><button type="submit" class="btn btn-danger">Konfirmasi</button></td>
+                                </tr>
+                            </table>
+                            </form>
+                        <span class='fa fa-print'></span> Cetak Tiket</a>
+                        <?php
                         }    
                     }
             ?>
